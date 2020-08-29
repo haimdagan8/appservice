c5633fb6943203d3ce07a26477c44cbecbee9bb1
haimdagan8 <52234923+haimdagan8@users.noreply.github.com>
2020-08-29 18:20:36 +0300

    Final deployment !

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return "Hello World - Final Version (Python)!"
+    return "Hello World - Final Deployment (Python)!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=443)
