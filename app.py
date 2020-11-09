10014a77802dd9d419152d664b423877f7d080c7
haimdagan8 <52234923+haimdagan8@users.noreply.github.com>
2020-11-09 13:25:53 +0200

    h

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return "Hello World - Production v1 (Python)!!"
+    return "Hello World - Production v1  (Python)!!"
 
 if __name__ == "__main__":
     app.run(debug=True, port=443)
